050cd368298dc659aa8ef3a6760283b142973460
Anand raja <meetanandns@gmail.com>
2022-12-16 08:34:52 +0000

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  overrides: {
+    files : ['*.js', '*.jsx'],
+    options: {
+      tabWidth: 2,
+      singleQuote: true,
+    }
+  }
+};
